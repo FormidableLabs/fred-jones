ff4340342c086b4158b04847eff6404123222e71
Eric Baer <me@ericbaer.com>
2015-03-25 12:07:47 -0700

    Removed needless bundling of assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,26 +24,6 @@ module.exports = function(grunt) {
         src: ['lib/assets/scripts/*.js']
       }
     },
-    uglify: {
-      'assets' : {
-        files : {
-          'lib/assets/scripts/bundles/core-bundle.js' : [
-            'lib/assets/scripts/vendor/jquery-1.8.3.min.js',
-            'lib/assets/scripts/vendor/lodash.min.js',
-            'lib/assets/scripts/vendor/raphael-min.js',
-            'lib/assets/scripts/vendor/morris.min.js',
-            'lib/assets/scripts/vendor/jquery.fittext.js',
-            'lib/assets/scripts/vendor/bootstrap-tooltip.js',
-            'lib/assets/scripts/vendor/bootstrap-popover.js'
-          ],
-          'lib/assets/scripts/bundles/codemirror.js' : [
-            'lib/assets/scripts/vendor/codemirror/codemirror.js',
-            'lib/assets/scripts/vendor/codemirror/javascript.js',
-            'lib/assets/scripts/vendor/codemirror/util/searchcursor.js'
-          ]
-        }
-      }
-    },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
@@ -81,7 +61,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-casper');
 
@@ -134,8 +113,6 @@ module.exports = function(grunt) {
     );
   });
 
-  grunt.registerTask('optimize', ['uglify']);
-  // Default task.
   grunt.registerTask('test', ['jshint', 'nodeunit', 'runtest', 'runbin', 'casper']);
   grunt.registerTask('default', ['test']);
 
