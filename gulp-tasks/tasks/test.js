08d2694427ef120a6228b9235f4851de810114b5
Eric Baer <me@ericbaer.com>
2015-03-26 15:27:52 -0700

    Clean before running tests

--- gulp-tasks/tasks/test.js
+++ gulp-tasks/tasks/test.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var nodeunit = require('gulp-nodeunit');
 var shell = require('gulp-shell')
+var del = require('del');
 var handleErrors = require('../util/handle-errors');
 
 gulp.task('nodeunit', function () {
@@ -11,12 +12,16 @@ gulp.task('nodeunit', function () {
     .on('error', handleErrors);
 });
 
-gulp.task('runtest', shell.task([
+gulp.task('test:clean', function() {
+  del(['tmp']);
+})
+
+gulp.task('runtest', ['test:clean'], shell.task([
   './bin/plato -q -d tmp -t "test report" test/fixtures/a.js test/fixtures/b.js test/fixtures/empty.js'
 ]));
 
-gulp.task('runbin', shell.task([
-  './bin/plato -q -l .jshintrc -x vendor -d reports -t "Plato report" lib/**'
+gulp.task('runbin', ['test:clean'], shell.task([
+  './bin/plato -q -l .jshintrc -x vendor -d tmp -t "Plato report" lib/**'
 ]));
 
 gulp.task('test', ['nodeunit', 'runtest', 'runbin']);
