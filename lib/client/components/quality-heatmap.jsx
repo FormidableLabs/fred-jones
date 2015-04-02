93f33816d663f3f0f7730fc20476fc40ec7bed37
Eric Baer <me@ericbaer.com>
2015-04-02 13:09:26 -0700

    Render Treemap by using D3 directly

--- lib/client/components/quality-heatmap.jsx
+++ lib/client/components/quality-heatmap.jsx
@@ -3,8 +3,22 @@
 var React = require('react');
 var d3 = require('d3');
 var _ = require('lodash');
-var Tile = require('./tile.jsx');
-var Treemap = require('./d3-graphs/treemap');
+
+var setDeepProperty = function(obj, path, value) {
+  var schema = obj;  // a moving reference to internal objects within obj
+  var length = path.length;
+
+  _.each(path, function(value, index) {
+    var pathSegment = path[index];
+    schema.children = schema.children || [];
+    if(!_.findWhere(schema.children, {name: pathSegment})) {
+      schema.children.push({name: pathSegment});
+    }
+    schema = _.findWhere(schema.children, {name: pathSegment});
+  })
+
+  schema.size = value;
+}
 
 var getScore = function(file) {
   var lintErrors = file.jshint.messages;
@@ -14,40 +28,78 @@ var getScore = function(file) {
   return lintErrors + cyclomaticComplexity + halsteadDifficulty;
 }
 
-var generateTreemapData = function(report) {
-  return _.map(report.reports, function(file) {
-    return {
-      label: file.info.fileShort,
-      value: getScore(file)
-    }
-  });
-}
+var formatData = function(data) {
+  var tree = {name: "ROOT"};
+  _.each(data.reports, function(fileReport) {
+    var path = fileReport.info.file
+      // TODO: use path.resolve to remove the ./ properly
+      .split('/').slice(1);
+    setDeepProperty(tree, path, getScore(fileReport));
+  })
+  return tree;
+};
 
-var QualityHeatmap = React.createClass({
-  getDefaultProps: function() {
-    return {
-      width: 600,
-      height: 300
-    }
+var color = d3.scale.category20c();
+
+var Cell = React.createClass({
+  getInitialState: function () {
+    return {}
   },
-
-  render: function() {
+  render: function () {
     return (
-      <Tile width={this.props.width}
-        height={this.props.height}
-        x={this.props.x}
-        y={this.props.y}>
-        <Treemap
-          width={450}
-          height={250}
-          title="Treemap"
-          data={generateTreemapData(this.props.report)}
-          textColor="#484848"
-          fontColor="12px"
-        />
-      </Tile>
-    );
+      <g transform={"translate(" + this.props.x + "," + this.props.y + ")"}>
+        //TODO: Tooltip with per-file info on hover
+        //TODO: Link to the real file on github
+        <title>{this.props.name}</title>
+        <rect
+          width={this.props.dx}
+          height={this.props.dy}
+          style={{
+            "fill": this.props.hasChildren ? color(this.props.name) : "none",
+            "stroke": "white",
+            "strokeWidth": "1.5px"
+          }}/>
+      </g>
+    )
   }
 });
 
-module.exports = QualityHeatmap;
+var Treemap = React.createClass({
+    getInitialState: function() {
+      var svgWidth = 475;
+      var svgHeight = 250;
+      var treemap = d3.layout.treemap().size([svgWidth, svgHeight]).sticky(true).value(function(d) { return d.size; });
+
+      var cells = treemap.nodes(formatData(this.props.report));
+      return {
+        svgWidth: svgWidth,
+        svgHeight: svgHeight,
+        cells: cells
+      }
+    },
+    drawCells: function () {
+      var cells = this.state.cells.map(function (cell, index) {
+        return (<Cell
+          pathSegment={index}
+          hasChildren={cell.children ? true : false}
+          name={cell.name}
+          x={cell.x}
+          y={cell.y}
+          dx={cell.dx}
+          dy={cell.dy}/>
+        ) })
+      return cells;
+    },
+    render: function() {
+        return (
+          <g
+            style={{"border": "2px solid black", "margin": "20px"}}
+            width={this.state.svgWidth}
+            height={this.state.svgHeight}>
+            {this.drawCells()}
+          </g>
+        )
+    }
+});
+
+module.exports = Treemap;
